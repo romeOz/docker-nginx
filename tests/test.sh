1e82911ecf743019a4b040ccb1fbf900bde3ea42
romeOz <serggalka@gmail.com>
2015-10-07 22:26:56 +0300

    Minor adjustments.

--- tests/test.sh
+++ tests/test.sh
@@ -5,55 +5,54 @@ set -e
 echo "-- Building Nginx stable"
 docker build -t nginx-stable ../stable/
 
-echo ""
+echo
 echo "-- Testing server is running"
 docker run --name app -d -p 8080:80 nginx-stable; sleep 5
 curl -I 127.0.0.1:8080 2>/dev/null | grep -wc '200 OK'
 
-echo ""
+echo
 echo "-- Clear"
 docker rm -f -v $(sudo docker ps -aq); sleep 5
 
-echo ""
+echo
 echo "-- Testing upstream"
 docker run --name app -d -v $(pwd)/app:/var/www/app romeoz/docker-phpfpm; sleep 5
 docker run --name nginx-test -d -v $(pwd)/sites-enabled:/etc/nginx/sites-enabled -p 8080:80 --link app:app nginx-stable; sleep 5
 curl 127.0.0.1:8080 2>/dev/null | grep -wc 'Hello world'
 
 
-echo ""
+echo
 echo "-- Clear"
 docker rm -f -v $(sudo docker ps -aq); sleep 5
 docker rmi -f nginx-stable
 
 
 
-
-echo ""
-echo ""
+echo
+echo
 echo "-- Building Nginx dev"
 docker build -t nginx-dev ../dev/
 
-echo ""
+echo
 echo "-- Testing server is running"
 docker run --name app -d -p 8080:80 nginx-dev; sleep 5
 curl -I 127.0.0.1:8080 2>/dev/null | grep -wc '200 OK'
 
-echo ""
+echo
 echo "-- Clear"
 docker rm -f -v $(sudo docker ps -aq); sleep 5
 
-echo ""
+echo
 echo "-- Testing upstream"
 docker run --name app -d -v $(pwd)/app:/var/www/app romeoz/docker-phpfpm; sleep 5
 docker run --name nginx-test -d -v $(pwd)/sites-enabled:/etc/nginx/sites-enabled -p 8080:80 --link app:app nginx-dev; sleep 5
 curl 127.0.0.1:8080 2>/dev/null | grep -wc 'Hello world'
 
 
-echo ""
+echo
 echo "-- Clear"
 docker rm -f -v $(sudo docker ps -aq); sleep 5
 docker rmi -f nginx-dev
 
-echo ""
+echo
 echo "-- Done"
